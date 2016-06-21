835b6fee2dbbb6798528a7eae6fa3b87ba6d5704
Matvey Kurzukov <mkurg@ya.ru>
2016-06-22 01:59:50 +0300

    Put the old correct script

--- exam/balance_genres.py
+++ exam/balance_genres.py
@@ -3,39 +3,32 @@
 
 import csv
 
-y1960 = 0
-y1970 = 0
-y1980 = 0
-y1990 = 0
-y2000 = 0
+publ = 0
+xud = 0
+uchn = 0
 
 
-with open("./tables/source_post1950_wordcount_by_genres.csv", encoding="utf-8") as csv_file:
+with open("./tables/source_post1950_wordcount_by_words.csv", encoding="utf-8") as csv_file:
 	reader = csv.DictReader(csv_file)
-	with open("./tables/source_post1950_wordcount_by_year.csv", "w", encoding="utf-8") as out_file:
+	with open("./tables/source_post1950_wordcount_by_genres.csv", "w", encoding="utf-8") as out_file:
 		writer = csv.DictWriter(out_file, fieldnames=reader.fieldnames)
 		writer.writeheader()
 		for row in reader:
 			try:
-				if row['created'].startswith('196'):
-					y1960 += int(row['words'])
-					if y1960 <= 20000000:
+				if row['sphere'].startswith('публицистика'):
+					publ += int(row['words'])
+					if publ <= 29000000:
 						writer.writerow(row)
-				elif row['created'].startswith('197'):
-					y1970 += int(row['words'])
-					if y1970 <= 20000000:
+				elif row['sphere'].startswith('художественная'):
+					xud += int(row['words'])
+					if xud <= 40000000:
 						writer.writerow(row)
-				elif row['created'].startswith('198'):
-					y1980 += int(row['words'])
-					if y1980 <= 20000000:
+				elif row['sphere'].startswith('учебно-научная'):
+					uchn += int(row['words'])
+					if uchn <= 12000000:
 						writer.writerow(row)
-				elif row['created'].startswith('199'):
-					y1990 += int(row['words'])
-					if y1990 <= 20000000:
-						writer.writerow(row)
-				elif row['created'].startswith('200'):
-					y2000 += int(row['words'])
-					if y2000 <= 20000000:
-						writer.writerow(row)
+				else:
+					writer.writerow(row)
 			except ValueError:
 				pass
+
